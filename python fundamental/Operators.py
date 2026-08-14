c4287155367542f87c4bdef310471794cd262ea6
Prasad <prasadap1409@gmail.com>
2026-08-14 09:29:28 +0530

    commit add file

---
+++ python fundamental/Operators.py
@@ -0,0 +1,66 @@
+#Arithmetic Operators ( + , -,*, / , % ,** )
+a=10
+b=7
+print("Addition:-",a+b) 
+print("subtraction:-",a-b)
+print("multiplication:-",a*b)
+print("divsion:-",a/b)
+print("floor divsion:-",a//b)
+print("modual:-",a%b)
+print("expoentiation:-",a**b)
+
+print("------------------------------------------------------------")
+#comparsion operators(==,<=,>=,<,>,!=)
+print("equal to:-",a==b)
+print("not equal to:-",a!=b)
+print("grater than:-",a>b)
+print("less than:-",a<b)
+print("grater than equal to:-",a>=b)
+print("less than equal to:-",a>=b)
+
+print("------------------------------------------------------------")
+#Assignment Operators(=,+=,-=,*=,%=,/=,//=,**=)
+c = 3
+print("assign:-", c)
+
+c = 3
+c += 2
+print("add and assign:-", c)
+
+c = 3
+c -= 2
+print("subtract and assign:-", c)
+
+c = 3
+c *= 2
+print("multipl and assign:-", c)
+
+c = 3
+c /= 2
+print("divsion and assign:-", c)
+
+c = 3
+c //= 2
+print("floor divsion and assign:-", c)
+
+c = 3
+c %= 2
+print("modual and assign:-", c)
+
+c = 3
+c **= 2
+print("expoentional and assign:-", c)
+
+print("------------------------------------------------------------")
+# Logical Operators(and ,or,not)
+x=5>9
+y=6<9
+print("logical and:-",x and y)
+print("logical or:-",x or y)
+print("logical not:-",not x)
+
+
+print("------------------------------------------------------------")
+# Operators precendence:-
+result=3+4*2
+print(result)
