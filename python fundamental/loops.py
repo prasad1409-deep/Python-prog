cf4e97f8fef880ea98ff3d2e54427af547fb85dc
Prasad <prasadap1409@gmail.com>
2026-08-22 23:58:36 +0530

    Create loops.py

---
+++ python fundamental/loops.py
@@ -0,0 +1,11 @@
+'''for i in range(1,6):
+    print(i)'''
+'''word="hello"
+for i in word:
+    print(i)'''
+'''for i in range(1,11):
+    print("3*",i,"=",3*i)'''
+total=0
+for i in range(1,6):
+     total +=i
+     print("sum=",total)  
